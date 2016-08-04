1062c24e3168187d72cc02dd4679a1a1125b6934
Cody <Cody@>
2016-08-04 15:26:18 -0500

    Hello World

---
+++ ProjectReal/ProjectReal/MainSource.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World!" << endl;
+
+	system("pause");
+
+	return 0;
+
+}
